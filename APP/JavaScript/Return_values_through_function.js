adfb8936c8e8c04387d8517fd3982e8f9a149cc0
sairam319 <sairamspeedup@gmail.com>
2023-07-25 15:19:52 +0530

    Return_values_function

---
+++ APP/JavaScript/Return_values_through_function.js
@@ -0,0 +1,29 @@
+let player1Time = 102;
+let player2Time = 107;
+
+// function getFastestRaceTime(){
+       
+//     if(player1Time<player2Time){
+
+//         return player1Time
+//     }else if(player1Time > player2Time){
+
+//         return player2Time;
+//     }else{
+
+//         return player1Time;
+//     }
+
+// }
+
+
+function totalRaceTime(){
+
+    return player1Time+player2Time;
+
+
+}
+
+let totalRaceTime = totalRaceTime();
+
+console.log(totalRaceTime);
