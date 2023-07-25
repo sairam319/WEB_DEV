29550ee8cf35ae68702fcacefd6bd3d686a54c3b
sairam319 <sairamspeedup@gmail.com>
2023-07-25 16:23:09 +0530

    Black_Jack_ver6

--- APP/Black_Jack/JS/black_jack.js
+++ APP/Black_Jack/JS/black_jack.js
@@ -1,21 +1,34 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard,secondCard];
-let sum = firstCard+secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message="";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
 function getRandomCard(){
+    
+    let randomNumber= Math.floor(Math.random()*13) + 1;
+      if(randomNumber > 10){
 
-    return 5;
+        return 10;
+      }
+      else if(randomNumber === 1){
+
+        return 11;
+      }else{
+
+        return randomNumber;
+      }
 }
 
 function startGame(){
-
+    isAlive = true;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards=[firstCard,secondCard];
+    sum = firstCard+secondCard;
     renderGame();
 }
 
