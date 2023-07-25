cc37643c6adabcfc7519ade2b7737180431c5e87
sairam319 <sairamspeedup@gmail.com>
2023-07-25 16:51:39 +0530

    logical_operators

---
+++ APP/JavaScript/logical_operator.js
@@ -0,0 +1,24 @@
+let hasSolvedChallenge = false;
+let hasHintsLeft = false;
+let likesDocumentries = true;
+let likeStartup = false;
+
+if(hasSolvedChallenge === false && hasHintsLeft === false){
+
+      showSolution(); 
+}
+
+if(likesDocumentries === true || likeStartup === true){
+
+    recommandMovie();
+    
+}
+function recommandMovie(){
+
+    console.log("Hey , check this new film !!");
+}
+
+function showSolution(){
+
+    console.log("Showing the Solution....");
+}
