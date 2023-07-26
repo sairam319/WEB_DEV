fc031a7fed927ae50c4c1320d3c48834f0a5ae4c
sairam319 <sairamspeedup@gmail.com>
2023-07-26 17:34:51 +0530

    Practice_code_12

---
+++ APP/JavaScript/practice_code_12.js
@@ -0,0 +1,10 @@
+let largestCountries = ["Tuvalu","India","USA","Indonesia","Monaco"]
+
+largestCountries.shift()  //taking out Tuvalu from left
+largestCountries.unshift("China") //placing China to the Left 
+largestCountries.pop()     //taking monaco out from right
+largestCountries.push("Pakistan")  //pushing pakistan to the right 
+
+
+
+console.log(largestCountries)
