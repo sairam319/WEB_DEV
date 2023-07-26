406ddba62ab73f9ae673c2dfdd3e484683409101
sairam319 <sairamspeedup@gmail.com>
2023-07-26 16:52:13 +0530

    Practice_code_9

---
+++ APP/JavaScript/objects_and_functions.js
@@ -0,0 +1,14 @@
+let person = {
+
+    name : "Sai Ram",
+    age : 29,
+    country : "INDIA"
+}
+
+function logData(){
+ 
+     console.log(person.name + " is " + person.age + " Years old and lives in " + person.country)
+
+}
+
+logData()
