f591bcfea50282d90c238e371ea12aa6e9642bad
sairam319 <sairamspeedup@gmail.com>
2023-07-26 16:05:37 +0530

    first_object

---
+++ APP/JavaScript/First_object.js
@@ -0,0 +1,9 @@
+ let castleListing = {
+
+         Name : "Harry Potter",
+         isAvailable : true,
+         siteSeeing : ["Lake","station"]
+ }
+
+ console.log(castleListing.isAvailable)
+ console.log(castleListing.siteSeeing)
