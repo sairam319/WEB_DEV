e3b1497bdbe5c89ac76d97dec97fce6d6ab64b6a
sairam319 <sairamspeedup@gmail.com>
2023-07-26 17:05:03 +0530

    practice_code_10

---
+++ APP/JavaScript/practice_code_10.js
@@ -0,0 +1,22 @@
+let age = 6;
+
+
+if(age <6){
+ 
+    console.log("free")
+
+}else if(age < 18){
+
+    console.log("Child Discount !!")
+
+}else if(age < 27){
+
+    console.log("Student Discount !!")
+
+}else if(age < 67){
+
+    console.log("Pay full price")
+}else{
+
+    console.log("Senior citizen discount")
+}
