1b975bfc9384f577f9991708c9a6e4c563f2c51e
sairam319 <sairamspeedup@gmail.com>
2023-07-26 17:23:54 +0530

    practice_code_11

---
+++ APP/JavaScript/practice_code_11.js
@@ -0,0 +1,9 @@
+let largeCountires = ["China","India","USA","Indonesia","Pakistan"]
+
+
+console.log("The 5 largest countries in the world :")
+for(let a=0; a<largeCountires.length; a++){
+
+   console.log(" - " + largeCountires[a])
+     
+}
