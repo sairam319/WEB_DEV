783cc3a0e96a5bf1882baa04fa266594a6b2e54a
sairam319 <sairamspeedup@gmail.com>
2023-08-01 14:42:09 +0530

    extension_ver3

--- APP/Chrome_Extension/JS/chrome_extension.js
+++ APP/Chrome_Extension/JS/chrome_extension.js
@@ -1,6 +1,19 @@
-let inputbtn = document.getElementById("input-btn")
+let myLeads = ["www.tiktok.com","www.ert.com","www.fgth.com"]
+const inputEl = document.getElementById("input-el")
+const inputbtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
 
 inputbtn.addEventListener("click" ,function() {
-
-    console.log("BUTTON CLICKED !!")
+     
+    
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
+
+
+for(let a = 0; a<myLeads.length ; a++){
+     
+    ulEl.textContent +=myLeads[a] + " "
+    // console.log(myLeads[a])
+}
