778635711e7ee0e9f98ef6a9e5c55001df5bac60
sairam319 <sairamspeedup@gmail.com>
2023-08-04 16:39:19 +0530

    practice_code_20

---
+++ APP/JavaScript/template_string_20.js
@@ -0,0 +1,10 @@
+//template String/Literals
+
+
+const recipient = "Krishna"
+const sender = "Sai"
+
+const prayLord = `Hey ! ${recipient} Hare Rama Hare ${recipient} from ${sender}`
+
+
+console.log(prayLord)
